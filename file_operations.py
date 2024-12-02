18a41df7f2ce2eafb74b73d225bd6afc00835ddf
jyugo0 <109700651+jyugo0@users.noreply.github.com>
2024-12-03 00:32:12 +0100

    Update file_operations.py

--- file_operations.py
+++ file_operations.py
@@ -36,34 +36,35 @@ def write_decoding_ratio(channel_name, ber, total_blocks,count_decoded,count_fai
     file.write(str(channel_name)+";"+ber+";"+str(Transmitter.block_size)+";"+str(total_blocks)+";"+str(count_decoded)+";"+str(count_failed)+";" + success_ratio+"\n")
     file.close()
 
-def write_noise_comparison_bits_for_GEC(start_state,k,b_no_err_to_good, h, total_bits, total_errors, total_blocks, avg_epr):
+def write_noise_comparison_bits_for_GEC(start_state,k,p_to_good,p_to_bad, h, total_bits, total_errors, total_blocks, avg_epr, is_interlaced):
     if not os.path.exists("noise_bits_for_GEC.csv"):
         create = open("noise_bits_for_GEC.csv", "w")
-        create.write("Starting State;k;Num of correct bits required to change channel;h;Total Bits;Total Bit Errors;Total blocks;Average Errors per Block\n")
+        create.write("Starting State;k;Basic probability to change channel to good;Basic probability to change channel to bad;h;Total Bits;Total Bit Errors;Total blocks;Average Errors per Block;Is interlaced? \n")
         create.close()
     file = open("noise_bits_for_GEC.csv","a")
     avg_epr = str(avg_epr).replace(".", ",")
-    file.write(start_state+";"+str(k)+";"+str(b_no_err_to_good)+";"+str(h)+";"+str(total_bits)+";"+str(total_errors)+";"+str(total_blocks)+";"+avg_epr+"\n")
+    file.write(start_state+";"+str(k)+";"+str(p_to_good)+";"+str(p_to_bad)+";"+str(h)+";"+str(total_bits)+";"+str(total_errors)+";"+str(total_blocks)+";"+avg_epr+";"+is_interlaced+"\n")
     file.close()
 
-def write_noise_comparison_bytes_for_GEC(start_state,k,b_no_err_to_good, h, total_bytes, total_errors, total_blocks, avg_epr):
+def write_noise_comparison_bytes_for_GEC(start_state,k,p_to_good, p_to_bad,h, total_bytes, total_errors, total_blocks, avg_epr, is_interlaced):
     if not os.path.exists("noise_bytes_for_GEC.csv"):
         create = open("noise_bytes_for_GEC.csv", "w")
-        create.write("Starting State;k;Num of correct bits required to change channel;h;Total Bytes;Total Byte Errors;Total blocks;Average Errors per Block\n")
+        create.write("Starting State;k;Basic probability to change channel to good;Basic probability to change channel to bad;h;Total Bytes;Total Byte Errors;Total blocks;Average Errors per Block;Is interlaced? \n")
         create.close()
     file = open("noise_bytes_for_GEC.csv","a")
     avg_epr = str(avg_epr).replace(".", ",")
-    file.write(start_state+";"+str(k)+";"+str(b_no_err_to_good)+";"+str(h)+";"+str(total_bytes)+";"+str(total_errors)+";"+str(total_blocks)+";"+avg_epr+"\n")
+    file.write(start_state+";"+str(k)+";"+str(p_to_good)+";"+str(p_to_bad)+";"+str(h)+";"+str(total_bytes)+";"+str(total_errors)+";"+str(total_blocks)+";"+avg_epr+";"+is_interlaced+"\n")
     file.close()
 
-def write_decoding_ratio_for_GEC(start_state,k,b_no_err_to_good, h, total_blocks,count_decoded,count_failed):
+def write_decoding_ratio_for_GEC(start_state,k,p_to_good,p_to_bad, h, total_blocks,count_decoded,count_failed, is_interlaced):
     if not os.path.exists("decoded_success_ratio_for_GEC.csv"):
         create = open("decoded_success_ratio_for_GEC.csv", "w")
-        create.write("Starting State;k;Num of correct bits required to change channel;h;Block size;Total Blocks;Decoded Blocks;Failed Blocks;Success Ratio\n")
+        create.write("Starting State;k;Basic probability to change channel to good;Basic probability to change channel to bad;h;Block size;Total Blocks;Decoded Blocks;Failed Blocks;Success Ratio;Is interlaced?\n")
         create.close()
 
     success_ratio=str(count_decoded/total_blocks).replace(".",",")
 
     file = open("decoded_success_ratio_for_GEC.csv", "a")
-    file.write(start_state+";"+str(k)+";"+str(b_no_err_to_good)+";"+str(h)+";"+str(Transmitter.block_size)+";"+str(total_blocks)+";"+str(count_decoded)+";"+str(count_failed)+";" + success_ratio+"\n")
+    file.write(start_state+";"+str(k)+";"+str(p_to_good)+";"+str(p_to_bad)+";"+str(h)+";"+str(Transmitter.block_size)+";"+str(total_blocks)+";"+str(count_decoded)+";"+str(count_failed)+";" + success_ratio+";"+is_interlaced+"\n")
     file.close()
+   
