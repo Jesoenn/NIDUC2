651961bd33d4674c2f4a9b36e66e7d8d33f919fc
jyugo0 <109700651+jyugo0@users.noreply.github.com>
2024-12-13 18:17:17 +0100

    update użycia GEC

--- main.py
+++ main.py
@@ -31,21 +31,41 @@ for chosen_channel in channels:
         testing.noise_comparison(transmitter.encoded_interlaced_bit_blocks, noise_bit_blocks,
                                  transmitter.encoded_byte_blocks, satellite.encoded_byte_blocks, chosen_channel,
                                  channel.bit_error_rate)
-"""
 states =["GOOD","BAD"]
 for chosen_state in states:
     gec = GEC()
-    for i in range(2):
-        # Simulacja przesylu bitow
-        noise_bit_255_interlaced_blocks = gec.simulation(chosen_state,transmitter.encoded_interlaced_bit_blocks)
-        satellite.receive_bit_blocks(noise_bit_255_interlaced_blocks)  # Odbiot blokow bitow
-        satellite.encoded_byte_blocks = satellite.bits_to_bytes(satellite.encoded_bit_blocks)  # zamiana bitow na bajty
-        satellite.encoded_byte_blocks = satellite.deinterlace(satellite.encoded_byte_blocks)  # Odwrocenie przeplotu
-        count_decoded, count_failed = satellite.decode("gec")  # policzenie ile dekodowan sie powiodlo, a ile nie
-        testing.decoder_success_rate_in_GEC_channel(chosen_state, gec.k,gec.b_no_err_to_good, gec.h, len(satellite.decoded_byte_blocks),
-                                     count_decoded, count_failed)
-        testing.noise_comparison_in_GEC_channel(transmitter.encoded_interlaced_bit_blocks, noise_bit_255_interlaced_blocks,
-                                 transmitter.encoded_byte_blocks, satellite.encoded_byte_blocks, chosen_state,
-                                                gec.k, gec.b_no_err_to_good, gec.h)
-"""
+    for transmission_type in transmission_types:
+        for i in range(1):
+            transmitter.prepare_to_transmit(transmission_type)
+
+            if transmission_type == "interlace":
+                noise_bit_blocks = gec.simulation(chosen_state,transmitter.encoded_interlaced_bit_blocks)
+                satellite.receive_bit_blocks(noise_bit_blocks, transmission_type, "gecInterlace")
+            else:
+                noise_bit_blocks = gec.simulation(chosen_state,transmitter.encoded_bit_blocks)
+                satellite.receive_bit_blocks(noise_bit_blocks, transmission_type, "gecNoInteralce")
+
+            if transmission_type == "interlace":
+                testing.decoder_success_rate_in_GEC_channel(chosen_state, gec.k, gec.p_to_good, gec.p_to_bad, gec.h,
+                                                            len(satellite.decoded_byte_blocks),
+                                                            satellite.count_decoded, satellite.count_failed, "true")
+                testing.noise_comparison_in_GEC_channel(transmitter.encoded_interlaced_bit_blocks,
+                                                        noise_bit_blocks,
+                                                        transmitter.encoded_byte_blocks, satellite.encoded_byte_blocks,
+                                                        chosen_state,
+                                                        gec.k, gec.p_to_good,gec.p_to_bad, gec.h, "true")
+                # test na występowanie przekłamanych bajtów obok siebie, w celu porównania działania przeplotu
+                testing.bits_group_errors_comparison( satellite.encoded_byte_blocks,transmitter.encoded_byte_blocks,"true")
+            else:
+                testing.decoder_success_rate_in_GEC_channel(chosen_state, gec.k, gec.p_to_good, gec.p_to_bad, gec.h,
+                                                            len(satellite.decoded_byte_blocks),
+                                                            satellite.count_decoded, satellite.count_failed, "false")
+                testing.noise_comparison_in_GEC_channel(transmitter.encoded_bit_blocks,
+                                                        noise_bit_blocks,
+                                                        transmitter.encoded_byte_blocks, satellite.encoded_byte_blocks,
+                                                        chosen_state,
+                                                        gec.k, gec.p_to_good, gec.p_to_bad, gec.h, "false")
+                # test na występowanie przekłamanych bajtów obok siebie, w celu porównania działania przeplotu
+                testing.bits_group_errors_comparison(satellite.encoded_byte_blocks,transmitter.encoded_byte_blocks,
+                                                     "false")
 
