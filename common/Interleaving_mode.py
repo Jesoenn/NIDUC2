e85930c1bc23a6eb880fcec490797a648a9a6e4c
Jesoenn <147878425+Jesoenn@users.noreply.github.com>
2024-12-19 12:28:57 +0100

    Enumy

---
+++ common/Interleaving_mode.py
@@ -0,0 +1,4 @@
+from enum import Enum
+class InterleavingMode(Enum):
+    WITH_INTERLEAVING = 1
+    WITHOUT_INTERLEAVING = 2
---
+++ common/channel_states.py
@@ -0,0 +1,5 @@
+from enum import Enum
+class ChannelStates(Enum):
+    GOOD = 1
+    MEDIUM = 2
+    BAD = 3
